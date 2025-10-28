87edd13a0f1f5116325ff2007706cd32ccc2e25d
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-28 02:46:53 +0100

    Créer le projet avec Maven et structure standard (src/main/java, resources) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SMR-3

---
+++ src/main/java/io/github/mahjoubech/smartlogiv2/SmartlogiV2Application.java
@@ -0,0 +1,13 @@
+package io.github.mahjoubech.smartlogiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlogiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartlogiV2Application.class, args);
+    }
+
+}
