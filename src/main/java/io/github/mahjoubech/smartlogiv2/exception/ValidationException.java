7d5f77395b67902555451f134167ea45498fef65
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-31 12:27:40 +0100

    Créer exceptions custom (ResourceNotFoundException, InvalidDataException):https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SMR-49

---
+++ src/main/java/io/github/mahjoubech/smartlogiv2/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package io.github.mahjoubech.smartlogiv2.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
