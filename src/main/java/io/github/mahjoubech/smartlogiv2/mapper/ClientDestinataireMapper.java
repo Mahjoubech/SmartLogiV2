0693519031d9ae03ac53e0affaa3c85c425639e4
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-31 04:31:18 +0100

    SMR-72 creer Mapper pour tout les Dtao ( Request et Responses) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SMR-12

--- src/main/java/io/github/mahjoubech/smartlogiv2/mapper/ClientDestinataireMapper.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/mapper/ClientDestinataireMapper.java
@@ -5,11 +5,21 @@ import io.github.mahjoubech.smartlogiv2.dto.response.ClientDestinataireResponse;
 import io.github.mahjoubech.smartlogiv2.model.entity.ClientExpediteur;
 import io.github.mahjoubech.smartlogiv2.model.entity.Destinataire;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ClientDestinataireMapper {
+
+    @Mapping(target = "id", ignore = true) // N'ignorew l'ID dyal Entity
+    @Mapping(target = "colis", ignore = true) // N'ignorew l'Relations
+    void updateDestinataire(ClientDestinataireRequest request, @MappingTarget Destinataire entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "colis", ignore = true)
+    void updateExpediteur(ClientDestinataireRequest request, @MappingTarget ClientExpediteur entity);
 
     ClientExpediteur toClientExpediteur(ClientDestinataireRequest dto);
     ClientDestinataireResponse toClientResponse(ClientExpediteur entity);
