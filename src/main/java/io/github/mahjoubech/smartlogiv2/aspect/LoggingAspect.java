ab89b624d184a2a741a9afe4ef1ae74d9c6fd2fe
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-31 14:37:14 +0100

    Ajouter la classe LoggingAspect pour la gestion des logs avec AOP : //charkaouielmahjoub50-1758013914627.atlassian.net/browse/SMR-76

--- src/main/java/io/github/mahjoubech/smartlogiv2/aspect/LoggingAspect.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/aspect/LoggingAspect.java
@@ -1,4 +1,17 @@
 package io.github.mahjoubech.smartlogiv2.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+    private final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+    // 4. AOP-T1.4 & AOP-T1.5 ghadi ykounou hna
 }
