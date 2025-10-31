264809ba4ed52b431efe01e65b1af88dca8b66e7
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-31 12:22:29 +0100

    Refactor ColisServiceImpl to use ResourceNotFoundException and ValidationException; add getAllowedValues method to ColisStatus enum

--- src/main/java/io/github/mahjoubech/smartlogiv2/model/enums/ColisStatus.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/model/enums/ColisStatus.java
@@ -1,10 +1,19 @@
 package io.github.mahjoubech.smartlogiv2.model.enums;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum ColisStatus {
     CREE,
     COLLECTE,
     EN_STOCK,
     EN_TRANSIT,
     LIVRE,
-    ANNULE
+    ANNULE;
+
+    public static String getAllowedValues() {
+        return Arrays.stream(ColisStatus.values())
+                .map(Enum::name)
+                .collect(Collectors.joining(", "));
+    }
 }
