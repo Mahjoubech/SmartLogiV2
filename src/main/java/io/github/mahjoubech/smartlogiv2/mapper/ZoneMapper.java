46768da2768c96515787144c1a0ec0b1e48f576a
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-31 04:31:18 +0100

    SMR-72 creer Mapper pour tout les Dtao ( Request et Responses) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SMR-12

--- src/main/java/io/github/mahjoubech/smartlogiv2/mapper/ZoneMapper.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/mapper/ZoneMapper.java
@@ -4,9 +4,14 @@ import io.github.mahjoubech.smartlogiv2.dto.request.ZoneRequest;
 import io.github.mahjoubech.smartlogiv2.dto.response.ZoneResponse;
 import io.github.mahjoubech.smartlogiv2.model.entity.Zone;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ZoneMapper {
     Zone toEntity(ZoneRequest dto);
     ZoneResponse toResponse(Zone entity);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "colis", ignore = true)
+    void updateEntityFromRequest(ZoneRequest request, @MappingTarget Zone entity);
 }
