ce11fa79a3439b632d0cb8e50d5789d8edc8faa7
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-31 04:29:55 +0100

    SMR-25 Ajouter méthodes custom (ex: findByVille, findByStatut, etc.) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SMR-24

--- src/main/java/io/github/mahjoubech/smartlogiv2/repository/ClientExpediteurRepository.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/repository/ClientExpediteurRepository.java
@@ -1,7 +1,10 @@
 package io.github.mahjoubech.smartlogiv2.repository;
 
 import io.github.mahjoubech.smartlogiv2.model.entity.ClientExpediteur;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientExpediteurRepository extends JpaRepository<ClientExpediteur, String> {
+    Page<ClientExpediteur> findByNomContainingIgnoreCaseOrPrenomContainingIgnoreCase(String nom, String prenom, Pageable pageable);
 }
--- src/main/java/io/github/mahjoubech/smartlogiv2/repository/LivreurRepository.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/repository/LivreurRepository.java
@@ -1,7 +1,10 @@
 package io.github.mahjoubech.smartlogiv2.repository;
 
 import io.github.mahjoubech.smartlogiv2.model.entity.Livreur;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LivreurRepository extends JpaRepository<Livreur, String> {
+    Page<Livreur> findByNomContainingIgnoreCaseOrPrenomContainingIgnoreCase(String key, String k, Pageable p);
 }
