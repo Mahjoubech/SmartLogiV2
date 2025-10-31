036ff227c1bc7459b38432aaff10d9172cc470da
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-31 04:29:55 +0100

    SMR-25 Ajouter méthodes custom (ex: findByVille, findByStatut, etc.) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SMR-24

--- src/main/java/io/github/mahjoubech/smartlogiv2/repository/ColisRepository.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/repository/ColisRepository.java
@@ -1,7 +1,18 @@
 package io.github.mahjoubech.smartlogiv2.repository;
 
 import io.github.mahjoubech.smartlogiv2.model.entity.Colis;
+import io.github.mahjoubech.smartlogiv2.model.entity.Livreur;
+import io.github.mahjoubech.smartlogiv2.model.enums.ColisStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ColisRepository extends JpaRepository<Colis,String> {
+
+    Page<Colis> findByStatut(ColisStatus statut, Pageable pageable);
+    Page<Colis> findByLivreurId(String livreurId, Pageable pageable);
+
 }
+
