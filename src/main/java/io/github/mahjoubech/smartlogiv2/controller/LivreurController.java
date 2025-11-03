81186225f2267717ebe62134f8c63db4c3037a3d
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-03 16:31:00 +0100

    test commit for rebase and merge main

--- src/main/java/io/github/mahjoubech/smartlogiv2/controller/LivreurController.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/controller/LivreurController.java
@@ -1,4 +1,12 @@
 package io.github.mahjoubech.smartlogiv2.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+        ("/api/v1/livreur")
+@RequiredArgsConstructor
 public class LivreurController {
 }
