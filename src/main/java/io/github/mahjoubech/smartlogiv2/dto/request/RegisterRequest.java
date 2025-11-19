952b60f8262f8476a50f4f276f9c86964f6a1711
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-19 02:28:56 +0100

    SMR-142 Créer les DTO de Login et DTO de register : Ajouter les classes LoginRequest et RegisterRequest pour la gestion des requêtes d'authentification

---
+++ src/main/java/io/github/mahjoubech/smartlogiv2/dto/request/RegisterRequest.java
@@ -0,0 +1,31 @@
+package io.github.mahjoubech.smartlogiv2.dto.request;
+
+import io.github.mahjoubech.smartlogiv2.model.entity.Zone;
+import io.github.mahjoubech.smartlogiv2.model.enums.Roles;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class RegisterRequest {
+    @NotBlank(message = "Le nom est obligatoire.")
+    private String nom;
+    @NotBlank(message = "Le Prenom est obligatoire.")
+    private String prenom;
+    @Email(message = "L'email doit être valide.")
+    private String email;
+    @NotBlank(message = "Telephone est obligatoire")
+    private String telephone;
+    @NotBlank(message = "password")
+    private String password;
+    @NotBlank(message = "La confirmation du mot de passe est obligatoire.")
+    private String confirmPassword;
+    @NotBlank(message = "Le rôle est obligatoire.")
+    private Roles role;
+    private String adresse;
+    private String vehicule;
+    private String zoneAssigned;
+
+}
