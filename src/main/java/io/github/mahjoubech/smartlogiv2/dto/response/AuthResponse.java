d681789d98da600f3c8e65a75312309d77aca305
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-19 02:29:34 +0100

    SMR-162 creer DTO de Auth response : Ajouter la classe AuthResponse pour gérer les réponses d'authentification

---
+++ src/main/java/io/github/mahjoubech/smartlogiv2/dto/response/AuthResponse.java
@@ -0,0 +1,9 @@
+package io.github.mahjoubech.smartlogiv2.dto.response;
+
+public class AuthResponse {
+    private String nomComplet;
+    private String email;
+    private String role;
+    private String token;
+
+}
