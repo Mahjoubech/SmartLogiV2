6c9b21556d3e262504ea10ca7287d3e75ebd96f8
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-19 01:26:31 +0100

    SMR-147 use spring JPA auditing pour Securise les donnes : Ajouter une classe abstraite BaseEntity pour la gestion des entités avec des champs d'audit

---
+++ src/main/java/io/github/mahjoubech/smartlogiv2/model/entity/BaseEntity.java
@@ -0,0 +1,38 @@
+package io.github.mahjoubech.smartlogiv2.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @CreatedDate
+    @Column(name = "date_creation", nullable = false, updatable = false)
+    private LocalDateTime dateCreation;
+
+    @LastModifiedDate
+    @Column(name = "date_modification")
+    private LocalDateTime dateModification;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private String modifiedBy;
+}
