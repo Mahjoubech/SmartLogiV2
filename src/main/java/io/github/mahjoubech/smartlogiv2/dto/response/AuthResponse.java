44754ef94414172db77de0a139e905340c8aadba
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-12-19 06:09:01 +0100

    Ajouter JwtService et mise à jour de AuthResponse : Implémenter la génération de token JWT et enrichir AuthResponse avec le token d'authentification

--- src/main/java/io/github/mahjoubech/smartlogiv2/dto/response/AuthResponse.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/dto/response/AuthResponse.java
@@ -1,5 +1,10 @@
 package io.github.mahjoubech.smartlogiv2.dto.response;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class AuthResponse {
     private String nomComplet;
     private String email;
