11df39e15c9da3222d200d7e8e4647e44a17ca6e
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-12-22 11:58:31 +0100

    Ajouter la gestion de l'auditeur : implémenter un bean AuditorAware pour récupérer le nom de l'utilisateur authentifié et renommer le champ createdAt en dateCreation dans PermissionResponseDetail.

--- src/main/java/io/github/mahjoubech/smartlogiv2/config/AppConfig.java
+++ src/main/java/io/github/mahjoubech/smartlogiv2/config/AppConfig.java
@@ -5,13 +5,19 @@ import io.github.mahjoubech.smartlogiv2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
@@ -36,5 +42,20 @@ public class AppConfig {
     @Bean
     public AuthenticationManager  authenticationManager(AuthenticationConfiguration  authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> {
+            Authentication authentication =
+                    SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication == null ||
+                    !authentication.isAuthenticated() ||
+                    authentication instanceof AnonymousAuthenticationToken) {
+                return Optional.empty();
+            }
+
+            return Optional.of(authentication.getName());
+        };
     }
 }
